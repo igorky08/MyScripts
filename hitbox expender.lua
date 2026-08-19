63f9f3513768fd13808f793fc7690b8e8e1c54dc
igoryoka <juanfoster850@gmail.com>
2026-08-19 20:29:39 +0200

    Refactor hitbox size and transparency logic

--- hitbox expender.lua
+++ hitbox expender.lua
@@ -334,7 +334,9 @@ RunService.RenderStepped:Connect(function()
 		end
 	elseif getgenv().HitboxStatus == true and getgenv().TeamCheck == true then
 		for i,v in next, Players:GetPlayers() do
-			if Players.LocalPlayer.Team ~= v.Team and v.Character and v.Character:FindFirstChild("Head") then
+			local localTeam = Players.LocalPlayer.Team
+			local enemyTeam = v.Team
+			if localTeam and enemyTeam and localTeam ~= enemyTeam then
 				pcall(function()
 					v.Character.Head.Size = Vector3.new(getgenv().HitboxSize, getgenv().HitboxSize, getgenv().HitboxSize)
 					v.Character.Head.Transparency = getgenv().HitboxTransparency
